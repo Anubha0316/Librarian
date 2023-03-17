4b203a92eeb371511b3cbe264ebae1b2b10bfc18
Anubha Gupta <77827642+Anubha0316@users.noreply.github.com>
2023-03-17 14:41:23 +0530

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="test">Welcome to Librarian</h1>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style scoped>
+  .test{
+    color: var(--primary);
+  }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="center">
+        <v-row>
+            <v-col cols="10" offset="1" sm="3" offset-md="4" offset-lg="3">
+                <v-avatar color="primary" size="95" tile>
+                    <v-img src="@/assets/logo.png"></v-img>
+                </v-avatar>
+                <v-text-field label="Username"></v-text-field>
+                <v-text-field label="Password" type="password"></v-text-field>
+                <v-btn class="base-inverted" block @click="authenticate()">
+                    Login
+                </v-btn>
+            </v-col>
+        </v-row>
+        <!-- <h1>Crazy Abubha Page</h1> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: "",
+            password: ""
+        }
+    },
+    methods: {
+        authenticate() {
+            alert("Login Successful");
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
